1e4f758ed8f03dd1481eaa2bb58b4163a875b59b
Jon Kroeker <jon.kroeker2@gmail.com>
2015-07-20 16:31:18 -0500

    Removed unused files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,10 @@ var plugin = require('gulp-load-plugins')({ lazy: true });
 var help = require('./tasks/task-help.js')(gulp);
 var config =  require('./tasks/helper-config.js')().getConfig();
 
-// Load up all of the custom tasks
+// Load up all of your custom gulp tasks here, each contained within their own file
 require('./tasks/task-photos.js')(gulp, config, plugin, help);
+
 
 // Default task is showing help
 gulp.task('default', ['help']);
-
-/*
-// get our config for use in all our tasks
-// hard wire our configuration helper right onto the plugin so we can pass it around
-// All our custom tasks
-require('./gulp/task-config.js')(gulp, config, plugin, help);
-require('./gulp/task-clean.js')(gulp, config, plugin, help);
-require('./gulp/task-lint.js')(gulp, config, plugin, help);
-require('./gulp/task-js.js')(gulp, config, plugin, help);
-require('./gulp/task-vendorJs.js')(gulp, config, plugin, help);
-require('./gulp/task-templates.js')(gulp, config, plugin, help);
-require('./gulp/task-coreCopy.js')(gulp, config, plugin, help);
-require('./gulp/task-css.js')(gulp, config, plugin, help);
-require('./gulp/task-vendorCss.js')(gulp, config, plugin, help);
-require('./gulp/task-watch.js')(gulp, config, plugin, help);
-require('./gulp/task-selectConfigXml.js')(gulp, config, plugin, help);
-require('./gulp/task-release.js')(gulp, config, plugin, help);
-require('./gulp/task-rebuild.js')(gulp, config, plugin, help);
-
-
-*/
-
 
